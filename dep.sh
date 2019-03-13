54b8c86a405776ba8a39ba43afc9efce41f8283b
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2019-03-13 03:42:18 -0400

    Update script template

--- dep.sh
+++ dep.sh
@@ -1,13 +1,28 @@
-# python
-sudo apt-get install python2.7
+#!/bin/bash
 
-# node
+# install python
+sudo apt-get -y install python2.7
+
+# install node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
+. .nvm/nvm.sh
 nvm install 11
 
+# clone repo
 git clone https://github.com/multiparty/jiff-benchmarking.git
 cd jiff-benchmarking
+git submodule init
+git submodule update
 
+# configure repo
 npm install
-# ./script.sh <parties> <replicas> <IP> <computation ID>
+# ./script.sh <parties> <replicas> <IP> <computation ID> &
+
+# for server do:
+# screen
+# <enter>
+# node server > out
+# ...
+# ...
+# Ctrl-A then Ctrl-D to detach screen
+# Ctrl-D to exist ssh
